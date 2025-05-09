a4afad3326be2acaf20cce469b2508f26b347540
Nriveraaa <202310167@fit.edu.ph>
2025-05-10 07:36:44 +0800

    feat (header): add header with nav links

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Students Grades</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="details.php">Details</a>
+        </nav>
+    </header>
 </body>
 </html>
