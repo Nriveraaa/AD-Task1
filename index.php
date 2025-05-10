6e386fa35238586db5cbbba27b8421103e40fbdc
Nriveraaa <202310167@fit.edu.ph>
2025-05-10 08:14:59 +0800

    feat(css link): add external css link for styling

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Students Grades</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
@@ -26,8 +27,9 @@
             for ($i = 0; i < count($students); $i++){
                 $status = "Fail";
                 if ($grades[$i] >=75) {
-                    $status = "Pass";
+                    $status = "Pass" ;
                 }
+                //display the results of their status
                 echo "<p>{$students[$i]}: {$grades[$i]} - <span class='" . strtolower($status) . "'>{$status}</span></p>";
             }
             ?>
